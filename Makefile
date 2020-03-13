90fb37c90559f7378595731874d3ecae10ea1265
ezhang7423 <ezhang7423@student.palomar.edu>
2020-03-13 14:25:20 -0700

    this is so cool

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 FLAGS=--std=c++11
 
 main: index.cpp future.o mantra.o reflection.o
-	g++ ${FLAGS} -o $^ $@
+	g++ -pthread ${FLAGS} -o $@ $^ && ./main
 
 future.o: future/main.cpp
-	g++ ${FLAGS} -c $^ $@
+	g++ ${FLAGS} -c $^ -o $@
 
 mantra.o: mantra/main.cpp
-	g++ ${FLAGS} -c $^ $@
+	g++ ${FLAGS} -c $^ -o $@
 
 reflection.o: reflection/main.cpp
-	g++ ${FLAGS} -c $^ $@
-
+	g++ ${FLAGS} -c $^ -o $@
 
 clean:
 	rm -rf *.o main
--- index.cpp
+++ index.cpp
@@ -4,11 +4,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <thread>
+#include <iostream>
 
 using namespace std;
 int main()
 {
-
+    int *status;
     thread fut(fut::main);
     fut.detach();
     thread man(man::main);
@@ -20,11 +21,13 @@ int main()
     {
         // do some stuff as child
         sleep(2);
+        cout << "lol im going to die now" << endl;
+        sleep(1);
         return 0;
     }
     else
     {
-        waitpid(ref);
+        waitpid(ref, status, WCONTINUED);
         cout << "the other guy finished so i can go now" << endl;
         return 0;
     }
